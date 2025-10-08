eaa38e86c3ecc7b13e702e7d08fbed438608bab4
Marshall Abrams <mabrams001@bham.rr.com>
2025-10-07 23:31:36 -0500

    Minor experiments

--- src/Main.purs
+++ src/Main.purs
@@ -5,7 +5,13 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (log)
 
+x :: String
+x = "this " <> "that"
+
 main :: Effect Unit
 main = do
-  log "🍝"
+        log "Yow!"
+        log x
+        log "🍝"
+
 
