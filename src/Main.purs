85b6ee6155b2ee2db461a65e005ca5aa3ad2ed1a
Marshall Abrams <mabrams001@bham.rr.com>
2025-10-10 23:45:19 -0500

    Added little tests

--- src/Main.purs
+++ src/Main.purs
@@ -1,17 +1,36 @@
 module Main where
 
 import Prelude
-
 import Effect (Effect)
 import Effect.Console (log)
+import Data.Number as N
+import Data.Int as I
 
-x :: String
-x = "this " <> "that"
+s :: String
+s = "this " <> "that"
+
+x :: Number
+x = 16.0
+
+n :: Int
+n = -4
+
+-- maybeint :: Maybe Int
+-- maybeint = fromNumber x
+
+nn :: Number
+nn = I.toNumber n
+
 
 main :: Effect Unit
 main = do
         log "Yow!"
-        log x
+        log s
+        log $ show $ I.pow 5 3
+        log $ show $ N.pow x nn
+        log $ show $ N.pow x 2.0
+        log $ show $ N.pow x (- nn)
+        log (show n)
         log "🍝"
 
 
