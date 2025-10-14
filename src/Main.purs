09412dcc58fc77b8ea8fc97b94a4db512288cd39
Marshall Abrams <mabrams001@bham.rr.com>
2025-10-14 00:44:05 -0500

    Minor experiments

--- src/Main.purs
+++ src/Main.purs
@@ -1,5 +1,7 @@
 module Main where
 
+-- If lsp complains that these don't exist, try running
+-- spago build or spago run.
 import Prelude
 import Effect (Effect)
 import Effect.Console (log)
@@ -13,7 +15,10 @@ x :: Number
 x = 16.0
 
 n :: Int
-n = -4
+n = -4 + -1
+
+z :: Int
+z = 27
 
 -- maybeint :: Maybe Int
 -- maybeint = fromNumber x
@@ -26,11 +31,11 @@ main :: Effect Unit
 main = do
         log "Yow!"
         log s
-        log $ show $ I.pow 5 3
+        log (show z)
+        log $ show $ I.pow 5 4
         log $ show $ N.pow x nn
-        log $ show $ N.pow x 2.0
+        log $ show $ N.pow x 3.0
         log $ show $ N.pow x (- nn)
+        log (show (N.pow 2.0 0.5))
         log (show n)
         log "🍝"
-
-
