3762d2e8e71a651a0795da8a28c68ebdb4a9e5ba
Marshall Abrams <mabrams001@bham.rr.com>
2025-10-24 21:26:04 -0500

    Commented out initial experiments, added successful random use

--- src/Main.purs
+++ src/Main.purs
@@ -10,6 +10,7 @@ import Data.List as L
 import Data.List.Lazy as LL
 import Effect.Random
 
+ricker :: Number → Number → Number → Number
 ricker k r n = n * exp (r * (1.0 - n/k))
 
 -- A lazy list iterate
@@ -20,7 +21,18 @@ seedless _ = random
 
 randnos = LL.iterate seedless (pure 0.0)
 
-main :: Effect Unit
+foo :: (Int * Number) -> Number -> String
+foo _ _ = ?foo
+
+{-
+main :: Effect Number
 main = do
         logShow $ LL.take 4 (ricks 1000.0 3.0 0.25)
-        map logShow $ LL.take 10 $ randnos
+        logShow $ map (LL.take 10) randnos
+        -}
+
+main :: Effect Unit
+main = do
+        randno <- random
+        logShow randno
+        pure unit
