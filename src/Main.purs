5e894b825b70418ba16da500984ff4e5c99f65b5
Marshall Abrams <mabrams001@bham.rr.com>
2025-10-24 22:23:56 -0500

    More experiments with random

--- src/Main.purs
+++ src/Main.purs
@@ -9,6 +9,9 @@ import Data.Array as A
 import Data.List as L
 import Data.List.Lazy as LL
 import Effect.Random
+-- import Jack  -- not needed?
+-- import Jack.Seed as JS
+-- import Jack.Rand as JR
 
 ricker :: Number → Number → Number → Number
 ricker k r n = n * exp (r * (1.0 - n/k))
@@ -19,10 +22,8 @@ ricks k r init = LL.iterate (ricker k r) init
 seedless :: Effect Number -> Effect Number
 seedless _ = random
 
-randnos = LL.iterate seedless (pure 0.0)
-
-foo :: (Int * Number) -> Number -> String
-foo _ _ = ?foo
+randeffects = LL.iterate seedless (pure 0.0)
+randeffects3 = LL.take 3 $ LL.iterate seedless (pure 0.0)
 
 {-
 main :: Effect Number
@@ -31,8 +32,16 @@ main = do
         logShow $ map (LL.take 10) randnos
         -}
 
+
 main :: Effect Unit
 main = do
         randno <- random
         logShow randno
         pure unit
+
+
+{-
+-- I thought I could use >>> which I think is like Haskell's >>, but doesn't work.
+main :: Effect Unit
+main = random >>= \randno -> logShow randno >>= \_ -> pure unit
+-}
