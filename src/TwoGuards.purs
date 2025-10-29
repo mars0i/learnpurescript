3d1912d9e6a8227a9f9ec044503d0c435ff66a54
Marshall Abrams <mabrams001@bham.rr.com>
2025-10-29 01:10:30 -0500

    Emptied Scratch, added new example TwoGuards.purs

---
+++ src/TwoGuards.purs
@@ -0,0 +1,23 @@
+-- Example using both versions of guard:
+module TwoGuards where
+
+import Prelude
+import Effect (Effect)
+import Effect.Console (log, logShow)
+import Data.Array
+import Data.EuclideanRing
+import Control.Alternative (guard) as A
+import Data.Monoid (guard) as M
+
+-- Example using both versions of guard:
+divBy3and5 :: Int -> Array Int
+divBy3and5 n = do
+  i <- 1..n
+  A.guard (i `mod` 3 == 0)
+  M.guard (i `mod` 5 == 0) (pure i)
+  
+
+
+main :: Effect Unit
+main = do
+       logShow $ divBy3and5 100
