66b6e438c0de007df52bc6d7fe8a20f7f7e2daa9
Marshall Abrams <mabrams001@bham.rr.com>
2025-10-31 14:05:12 -0500

    Added failed experiments

--- src/TwoGuards.purs
+++ src/TwoGuards.purs
@@ -5,19 +5,35 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (log, logShow)
 import Data.Array ((..)) -- i.e. import the range operator: ..
+import Data.Maybe
 import Data.EuclideanRing (mod)
 import Control.Alternative (guard) as A
 import Data.Monoid (guard) as M
 
 -- Example using both versions of guard:
-divBy3and5 :: Int -> Array Int
-divBy3and5 n = do
+divArrBy3and5 :: Int -> Array Int
+divArrBy3and5 n = do
   i <- 1..n
   A.guard (i `mod` 3 == 0)
   M.guard (i `mod` 5 == 0) (pure i)
+
+{- M.guard doesn't seem to work with Maybe. I thought it would. (A.guard works.)
+   I think Maybe isn't an instance of Monoid.
+divMaybeBy3and5 :: Maybe Int -> Maybe Int
+divMaybeBy3and5 n = do
+  i <- n
+  A.guard (i `mod` 3 == 0)
+  M.guard (i `mod` 5 == 0) (pure i)
+  -}  
   
+{-
+Failed experiment:
+dividem :: Array Int -> Array Int -> Array Int
+dividem xs ys =
+        [ (\x y -> M.guard (y /= 0) (x / y)) ] <*> xs <*> ys
+-}
 
 
 main :: Effect Unit
 main = do
-       logShow $ divBy3and5 100
+       logShow $ divArrBy3and5 100
